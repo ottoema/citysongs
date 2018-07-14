1b41b108939fefa4ea82c77abe5a9c66c3f8b30d
Emanuelsson, Otto <otto.emanuelsson@volvocars.com>
2018-07-14 14:39:57 +0200

    Adding model

--- webserver/hello.py
+++ webserver/hello.py
@@ -1,3 +1,11 @@
-print "Content-Type: text/plain"
-print ""
-print "Congratulations, it's a web app!"
+import webapp2
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers["Content-Type"] = "text/plain"
+        self.response.write("Congratulations, it's a web app!")
+
+routes = [('/', MainPage)]
+
+my_app = webapp2.WSGIApplication(routes, debug=True)
